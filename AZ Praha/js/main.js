2712d0d938044bf078ebee6293a3966f47987c24
kenakata <49375477+kenakata@users.noreply.github.com>
2019-04-21 15:55:59 +0300

    main.js

--- AZ Praha/js/main.js
+++ AZ Praha/js/main.js
@@ -12,7 +12,6 @@ function getTheBiggestHeight() {
 	}
 
 	var sortedArrayHeight = arrayHeight.sort(sortArrayHeight);
-
 	
 	for(var i = 0; imgContainers.length > i; i++) {
 		imgContainers[i].style.setProperty('height',  sortedArrayHeight[0]  + 'px');
