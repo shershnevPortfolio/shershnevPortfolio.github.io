cf9e0a29f8aa99567ba2a524e98c6cfaca8e0711
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-03 15:38:08 +0300

    commit

--- selfEducation/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import './404';
-import initHome from './home';
-// import initExample from './example'; // need remove;
-
-export default () => {
-  console.log('init pages');
-  initHome();
-  // initExample();
-};
