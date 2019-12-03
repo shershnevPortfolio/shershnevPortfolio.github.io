44ffdb025dd2683016a727805ff0b195a2d5d87f
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-03 15:22:13 +0300

    commit

---
+++ selfEducation/src/layout/index.js
@@ -0,0 +1,12 @@
+import './base';
+import './grid';
+import './wrapper';
+import './content';
+import  initLayout from './header';
+import './header';
+import './footer';
+
+export default () => {
+  initLayout();
+}
+
